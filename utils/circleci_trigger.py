be42394a70718538e31f2e95774b9f5a15ac8f58
ydshieh <ydshieh@users.noreply.github.com>
2025-04-30 12:13:21 +0200

    try

---
+++ utils/circleci_trigger.py
@@ -0,0 +1,21 @@
+import http.client
+import os
+
+conn = http.client.HTTPSConnection("circleci.com")
+
+pr_number = "37885"
+payload = {"branch": f"pull/{pr_number}/head" , "parameters" :{"nightly": "false"}}
+payload = str(payload).replace("'", '"').replace('"false"', 'false')
+print(payload)
+
+headers = {
+    'content-type': "application/json",
+    'Circle-Token': os.environ.get("CIRCLECI_TOKEN", ""),
+}
+
+conn.request("POST", "/api/v2/project/gh/huggingface/transformers/pipeline", payload, headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
